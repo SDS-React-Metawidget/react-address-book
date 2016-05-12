dae327dca4d54b75dafc7353d488c8197aa98fca
Ryan Soury <ryansoury@Ryans-MacBook-Pro-2.local>
2016-05-12 19:34:21 +1000

    add new contact

---
+++ src/app/views/AddContact/index.js
@@ -0,0 +1,22 @@
+import React, { Component, PropTypes } from 'react';
+import { asyncConnect } from 'redux-async-connect';
+import { connect } from 'react-redux';
+import Helmet from 'react-helmet';
+import { Link } from 'react-router';
+
+import { selectContact } from 'app/actions/contact';
+import ContactForm from 'app/components/ContactForm';
+
+// import styles from './styles.styl';
+export default class EditContactView extends Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+        <Helmet title="Creating new contact | React Address Book" />
+        <Link to="/">Home</Link>
+        <ContactForm />
+        
+      </div>
+    );
+  }
+}

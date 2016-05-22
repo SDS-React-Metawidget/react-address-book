e13d62b678ba1a565cb8e68076abfda4e7cf8940
Jeremy Heritage <jheritage@gmail.com>
2016-05-22 21:59:13 +1000

    setting up the basis of View Contact Page for further editing

--- src/app/views/Contact/index.js
+++ src/app/views/Contact/index.js
@@ -29,14 +29,33 @@ export default class ContactView extends Component { // eslint-disable-line reac
     contact: PropTypes.object,
   };
 
+  /*
+    Select Bootstrap HTML elements to structure the view contact template.
+    Remember, you virtually look up a component on their website and copy
+    and paste the code...first look over the components and think of which
+    ones would work together in this template to display the 'contact'.
+
+    Here an example of HTML being populated by the current available 'contact'
+    attributes
+    <td>
+      <span><Link to={`/contact/${id}`}>{lastName}, {firstName}</Link></span>
+    </td>
+    <td>{details}</td>
+    
+    Keep the Helment element and try adding Bootstrap button classes to the Link
+    elements.
+  */
   render() {
     const { contact } = this.props;
     return (
       <div>
+        <div className="page-header">
+          <h1>Contact</h1>
+        </div>
         <Helmet title="Viewing contact | React Address Book" />
-        <Link to="/">Home</Link>
         <Contact {...contact} />
         <Link to={`/contact/${contact.id}/edit`}>Edit</Link>
+        <Link to="/">Home</Link>
       </div>
     );
   }

a76e8c1d061ca108a95e6c351d79ed0a382b228c
Jeremy Heritage <jheritage@gmail.com>
2016-06-02 17:07:53 +1000

    first implmentation of bootstrap layout for view contact

--- src/app/views/Contact/index.js
+++ src/app/views/Contact/index.js
@@ -44,18 +44,91 @@ export default class ContactView extends Component { // eslint-disable-line reac
     
     Keep the Helment element and try adding Bootstrap button classes to the Link
     elements.
+
+    <Contact {...contact} />
   */
   render() {
     const { contact } = this.props;
     return (
       <div>
-        <div className="page-header">
-          <h1>Contact</h1>
-        </div>
         <Helmet title="Viewing contact | React Address Book" />
-        <Contact {...contact} />
-        <Link to={`/contact/${contact.id}/edit`}>Edit</Link>
-        <Link to="/">Home</Link>
+
+        <div className="container">
+          <div className="row">
+            
+         
+
+            <div className="row page-header">
+              
+              <div className="col-md-4 text-center">
+                <div className="panel panel-default">
+                  <div className="panel-body">
+                      <span className="glyphicon glyphicon-user" aria-hidden="true"></span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-md-8">
+                <h1>{contact.firstName} {contact.lastName} <small>Business</small>
+                </h1>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div className="row">
+
+              <div className="row">
+                <div className="col-md-4 text-right"><p><strong>Title:</strong></p></div>
+                <div className="col-md-8"><p>Mr.</p></div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-4 text-right"><p><strong>Name:</strong></p></div>
+                <div className="col-md-8"><p>{contact.firstName} {contact.lastName}</p></div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-4 text-right"><p><strong>Type:</strong></p></div>
+                <div className="col-md-8"><p>Business</p></div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-4 text-right"><p><strong>Phone Number:</strong></p></div>
+                <div className="col-md-8"><p>000 000 000</p></div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-4 text-right"><p><strong>Email Address:</strong></p></div>
+                <div className="col-md-8"><p>{contact.details}</p></div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-4 text-right"><p><strong>Address:</strong></p></div>
+                <div className="col-md-8">
+                  <p>
+                    <address>15 Broadway,<br/>
+                      Ultimo NSW 2007
+                    </address>
+                  </p>
+                </div>
+              </div>
+
+              <hr/>
+
+          </div>
+
+        </div>
+        <Link className="btn btn-default" to={`/contact/${contact.id}/edit`}>
+          <span className="glyphicon glyphicon-pencil" aria-hidden="true"></span>&nbsp;
+          <span>Edit</span>
+        </Link>
+        &nbsp;
+        <Link className="btn btn-default" to="/">
+          <span className="glyphicon glyphicon-home" aria-hidden="true"></span>&nbsp;
+          <span>Home</span>
+        </Link>
       </div>
     );
   }

9fc10aa934e952bb856fd545d33ff3c17a0ce130
Nathan Hardy <nhardy@live.com.au>
2016-06-09 01:35:30 +1000

    Some code adjustments

--- src/app/components/ContactForm/index.js
+++ src/app/components/ContactForm/index.js
@@ -3,7 +3,8 @@ import { findDOMNode } from 'react-dom';
 import { connect } from 'react-redux';
 
 import { addContact, updateContact } from 'app/actions/contacts';
-import styles from './cool.styl';
+
+import styles from './styles.styl';
 
 @connect(null, {
   addContact,
@@ -24,7 +25,8 @@ export default class SearchPanel extends Component {
     updateContact: PropTypes.func,
   };
 
-  save = () => {
+  save = (e) => {
+    e.preventDefault();
     const firstName = findDOMNode(this.refs.fname).value;
     const lastName = findDOMNode(this.refs.lname).value;
     const details = findDOMNode(this.refs.details).value;
@@ -34,19 +36,14 @@ export default class SearchPanel extends Component {
     const phone = findDOMNode(this.refs.phone).value;
     const address = findDOMNode(this.refs.address).value;
 
-    //New fields
-    const stuff = { firstName, lastName, details, type, title, email, phone, address };
-    if(this.props.id) {
-      this.props.updateContact(this.props.id, stuff);
+    // New fields
+    const fields = { firstName, lastName, details, type, title, email, phone, address };
+    if (this.props.id) {
+      this.props.updateContact(this.props.id, fields);
     } else {
-      const ret = this.props.addContact(stuff);
-      alert(JSON.stringify(ret));
-      console.log(ret);
+      this.props.addContact(fields);
     }
-
   }
-
-
 
   render() {
     const { firstName, lastName, details, type, title, email, phone, address } = this.props;
@@ -55,22 +52,22 @@ export default class SearchPanel extends Component {
         <div className="row">
           <div className="form-group col-md-4">
             <label htmlFor="fname">First name:</label>
-            <input className={styles.form_input} id="fname" ref="fname" type="text" value={firstName} />
+            <input className={styles.input} id="fname" ref="fname" type="text" value={firstName} />
           </div>
 
           <div className="form-group col-md-4">
             <label htmlFor="lname">Last name:</label>
-            <input className={styles.form_input} id="lname" ref="lname" type="text" value={lastName} />
+            <input className={styles.input} id="lname" ref="lname" type="text" value={lastName} />
           </div>
 
-          <div className="form-group col-md-4"> 
-            <label htmlFor="title">Title:</label> 
-            <select className={styles.form_select} id="title" ref="title" defaultValue={title || `Miss`}>
+          <div className="form-group col-md-4">
+            <label htmlFor="title">Title:</label>
+            <select className={styles.input} id="title" ref="title" defaultValue={title || 'Miss'}>
               <option value="Mr">Mr</option>
               <option value="Mrs">Mrs</option>
               <option value="Miss">Miss</option>
               <option value="Ms" >Ms</option>
-            </select> 
+            </select>
           </div>
 
         </div>
@@ -78,31 +75,33 @@ export default class SearchPanel extends Component {
         <div className="row">
           <div className="form-group col-md-4">
             <label htmlFor="contacttype">Contact Type:</label>
-            <select className={styles.form_select} id="type" ref="type" defaultValue={type}>
+            <select className={styles.input} id="type" ref="type" defaultValue={type}>
               <option value="Personal" >Personal</option>
               <option value="Business" >Business</option>
             </select>
           </div>
           <div className="form-group col-md-4">
             <label htmlFor="email">Email:</label>
-            <input className={styles.form_input} id="email" ref="email" type="text" value={email} />
+            <input className={styles.input} id="email" ref="email" type="text" value={email} />
           </div>
           <div className="form-group col-md-4">
             <label htmlFor="phone">Phone:</label>
-            <input className={styles.form_input} id="phone" ref="phone" type="text" value={phone} />
+            <input className={styles.input} id="phone" ref="phone" type="text" value={phone} />
           </div>
         </div>
 
         <div>
           <label htmlFor="address">Address:</label>
-          <input className={styles.form_input} type="text" id="address" ref="address" value={address} />
+          <input className={styles.input} type="text" id="address" ref="address" value={address} />
         </div>
         <br />
         <label htmlFor="details">Details:</label>
-        <textarea id="details" ref="details" className={styles.form_field} placeholder={`Describe yourself... make it brief... we don't want to know too much...\nSimple things...\n\nSo, what's your bank details?`}>{details}</textarea>
+        <textarea id="details" ref="details" className={styles.field} placeholder={`Describe them...\nmake it brief...`}>
+          {details}
+        </textarea>
         <br />
         <div className={styles.button_wrapper}>
-          <button className={`btn btn-default`} onClick={this.save}>Save</button>
+          <button className="btn btn-default" onClick={this.save}>Save</button>
         </div>
       </form>
     );
